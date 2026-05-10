a3510fe7e504bc9af5ba0ed18a574cdfdbd9aa5f
Aaloka123 <aalokapoudel10@gmail.com>
2026-05-10 23:34:12 +0545

    Add initial backend setup with Maven configuration, application properties, and basic Spring Boot structure. Include .gitignore and .gitattributes for environment consistency, and implement the Mednexus application with a main class and test cases.

---
+++ Backend/src/main/java/com/mednexus/mednexus/MednexusApplication.java
@@ -0,0 +1,13 @@
+package com.mednexus.mednexus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MednexusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MednexusApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/mednexus/mednexus/MednexusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mednexus.mednexus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MednexusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
